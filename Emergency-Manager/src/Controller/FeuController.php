81598ca3f2d2162926a073b0c8008416a87def98
mcieslar <mathieu.cieslar@orange.com>
2024-12-20 16:39:41 +0100

    webSocketWorked

--- Emergency-Manager/src/Controller/FeuController.php
+++ Emergency-Manager/src/Controller/FeuController.php
@@ -2,43 +2,32 @@
 
 namespace App\Controller;
 
-use AllowDynamicProperties;
-use App\Entity\Feu;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 
-#[AllowDynamicProperties]
 class FeuController extends AbstractController
 {
-    public function __construct(
-
-    ) {
-        $this->status = "ko";
+    #[Route('/api/feu', name: 'get_feu', methods: ['GET'])]
+    public function getAllFeu(EntityManagerInterface $em): JsonResponse
+    {
+        $data = $em->getRepository(Feu::class)->findAll();
+        return $this->json($data);
     }
 
-    #[Route(path: '/api/feu', name: 'get_feu', methods: "GET")]
-    public function getAllFeu(Request $request, EntityManagerInterface $em ): JsonResponse {
-$data = $em->getRepository(Feu::class)->findAll();
-            return $this->json(
-               $data
-            );
+    #[Route('/api/update', name: 'get_update', methods: ['GET'])]
+    public function notifyNewFeu(HubInterface $hub): JsonResponse
+    {
+        // Créer un événement pour Mercure
+        $update = new Update(
+            'https://example.com/new-fire', // Sujet unique
+            json_encode(['coorX' => 46.764, 'coorY' => 4.835, 'nom' => 'Feu simulé'])
+        );
+        // Envoyer l'événement au Hub Mercure
+        $hub->publish($update);
+        return new JsonResponse(['status' => 'success']);
     }
-
-    #[Route(path: '/api/feu', name: 'post_feu', methods: "POST")]
-    public function postFeu(Request $request, EntityManagerInterface $em ): JsonResponse {
-        $data = $request->toArray();
-
-
-
-
-
-    }
-
-
 }
